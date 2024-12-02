a7381f2e75cae181e64615709657aab250cf8286
kolon0 <whatamnotsaying@tutanota.de>
2024-12-02 22:18:35 +0300

    Update pure_blake3_cipher.py

--- pure_blake3_cipher.py
+++ pure_blake3_cipher.py
@@ -2,40 +2,56 @@ import secrets
 from blake3 import blake3
 import hmac
 
+class MACMismatchException(Exception):
+    pass
+
 def init_blake3_cipher(key):
     def derive_key(context):
         return blake3(key, derive_key_context=context).digest()
 
     key_ciphertext = derive_key("ciphertext")
     key_mac = derive_key("mac")
-    # We seperate keys of encryption and mac with key derivation feature of blake3.
+    # we seperate keys of encryption and mac
+    # blake3 is also kdf
 
-    def encrypt(plaintext): # If plaintext is a string, you have to encode it before using that function. For example: plaintext.encode("utf-8")
+    def encrypt(plaintext):
+        # if you don't want assertions to run, use python's -O flag
+        assert type(plaintext) == bytes, "plaintext must be bytes type"
+        assert len(plaintext) > 0, "plaintext length must be bigger than zero"
+        
         nonce = secrets.token_bytes(24)
-        # We always generate a random 192-bit nonce for every given plaintext. Hence, it is practically impossible to generate same nonce twice.
-
+        # random 192-bit nonce for every plaintext
+        
         keystream = blake3(nonce, key=key_ciphertext).digest(length=len(plaintext))
-        # The keystream is same length as plaintext. Thanks to the blake3's XOF feature.
+        # blake3 is also xof
         
         ciphertext = bytes([p ^ k for p, k in zip(plaintext, keystream)])
-        # It is just doing XOR with plaintext and keystream.
-    
+        # xor plaintext with keystream
+        
         mac = blake3(nonce + ciphertext, key=key_mac).digest()
-        # It is for validating given nonce and ciphertext is not modified by Eve.
-
+        # to detect modification of ciphertext, nonce or mac
+        
         return (ciphertext, mac, nonce)
 
     def decrypt(ciphertext, given_mac, nonce):
+        assert type(ciphertext) == bytes, "ciphertext must be bytes type"
+        assert type(given_mac) == bytes, "mac must be bytes type"
+        assert type(nonce) == bytes, "nonce must be bytes type"
+        assert len(ciphertext) > 0, "ciphertext length must be bigger than zero"
+        assert len(given_mac) == 32, "mac length must be 32"
+        assert len(nonce) == 24, "nonce length must be 24"
+        
         calculated_mac = blake3(nonce + ciphertext, key=key_mac).digest()
 
         if not hmac.compare_digest(calculated_mac, given_mac):
-            raise Exception("Sorry, the given mac is not matching the calculated mac.")
-
+            raise MACMismatchException("mac mismatched")
+            # possible mitm?
+            
         keystream = blake3(nonce, key=key_ciphertext).digest(length=len(ciphertext))
-        # We generate same keystream that is used to encrypt the plaintext.
+        # we generate the same keystream that is used for encrypt the plaintext
         
         plaintext = bytes([c ^ k for c, k in zip(ciphertext, keystream)])
-        # It is just doing XOR with ciphertext and keystream.
+        # xor ciphertext with keystream
 
         return plaintext
 
